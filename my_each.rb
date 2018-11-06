497ad209fd3c74ef5eae689cc17500524aca9288
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-06 22:32:16 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,14 @@
-def my_each (array)
-  index = 0
-  while index < array.length
-    array[index] = yield(array[index])
-    index += 1
+def my_each (arr)
+  i = 0
+  while i < arr.length
+    yield(arr[i])
+    i += 1
   end
-  array
+  arr
+end
+
+arr = [1,2,3,4]
+
+my_each(arr) do |i|
+  puts "#{i}"
 end
